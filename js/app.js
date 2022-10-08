a54b11d2fec0b17e502e230e0a6fb3d582aa3876
JinHo <dlwlsghgk124@naver.com>
2022-10-08 11:36:36 +0900

    window onload event fix

--- js/app.js
+++ js/app.js
@@ -1,25 +1,3 @@
-const savedUsername = getStorage(USERNAME_KEY);
-const userBookmarks = JSON.parse(getStorage(BOOKMARK_KEY));
-
-/* Set Initial Bookmark */
-if (userBookmarks === null || !userBookmarks.length) {
-    setBookmark();
-}
-
-if (savedUsername === null) {
-    // show the form
-    // else hidden
-    loginForm.classList.remove(HIDDEN_CLASS);
-    addEvent(loginForm, 'submit', onLoginSubmit);
-} else {
-    // hide the form
-    // else show
-    loginForm.classList.add(HIDDEN_CLASS);
-    paintGreeting(savedUsername);
-    paintBookmark(JSON.parse(getStorage(BOOKMARK_KEY)));
-    paintTodo(getTodo());
-    paintQuote();
-}
 function handleResizeElements(e) {
     const innerWidth = e.currentTarget.innerWidth;
     const asideLastChild = selector(document)('#main-content aside:last-child');
@@ -33,5 +11,30 @@ function handleResizeElements(e) {
         appendChild(asideLastChild, asideChilds);
     }
 }
+function windowOnload(e) {
+    const savedUsername = getStorage(USERNAME_KEY);
+    const userBookmarks = JSON.parse(getStorage(BOOKMARK_KEY));
+
+    /* Set Initial Bookmark */
+    if (userBookmarks === null || !userBookmarks.length) {
+        setBookmark();
+    }
+
+    if (savedUsername === null) {
+        // show the form
+        // else hidden
+        loginForm.classList.remove(HIDDEN_CLASS);
+        addEvent(loginForm, 'submit', onLoginSubmit);
+    } else {
+        // hide the form
+        // else show
+        loginForm.classList.add(HIDDEN_CLASS);
+        paintGreeting(savedUsername);
+        paintBookmark(JSON.parse(getStorage(BOOKMARK_KEY)));
+        paintTodo(getTodo());
+        paintQuote();
+    }
+    handleResizeElements(e);
+}
 addEvent(window, 'resize', handleResizeElements);
-addEvent(window, 'load', handleResizeElements);
+addEvent(window, 'load', windowOnload);
